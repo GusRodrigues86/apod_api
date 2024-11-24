3bd799f7b1becc18c2fbf44b164aebf2b2242427
GusRodrigues86 <omykronbr@gmail.com>
2024-11-24 16:27:42 -0500

    brute config

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <glog/logging.h>
 
 int main(int argc, char *argv[])
 {
-   std::cout << "Hello to the APOD API" << std::endl;
+   google::InitGoogleLogging(argv[0]);
+   google::SetLogFilenameExtension(".log");
+   // FLAGS_logtostderr = 1;
+   FLAGS_log_dir = ".";
+   FLAGS_timestamp_in_logfile_name = false;
+   FLAGS_logfile_mode = 0;
+   LOG(INFO) << "Found 0 cookies";
+   // std::cout << "Hello to the APOD API" << std::endl;
+   // FLAGS_logto = 1;
+   LOG(INFO) << "Found 1 cookies";
    return 0;
 }
