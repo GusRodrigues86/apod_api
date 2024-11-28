32d0ee318ee522ce6614c82c5a249b347f606d4c
GusRodrigues86 <omykronbr@gmail.com>
2024-11-27 21:30:25 -0500

    yeah, not blocking!

--- src/app.cpp
+++ src/app.cpp
@@ -2,60 +2,104 @@
 #include <thread>
 #include <iostream>
 #include <sstream>
-#include "oatpp/core/macro/component.hpp"
-#include "oatpp/core/base/Environment.hpp"
-#include "oatpp/network/Server.hpp"
-#include "oatpp/network/tcp/server/ConnectionProvider.hpp"
-#include "oatpp/web/server/HttpConnectionHandler.hpp"
 
-#include "Logger/logger.h"
+#include "app.h"
+#include <server_components.hpp>
 
-void run() {
-	/* Create Router for HTTP requests routing */
-	auto router = oatpp::web::server::HttpRouter::createShared();
-
-	/* Create HTTP connection handler with router */
-	auto connectionHandler = oatpp::web::server::HttpConnectionHandler::createShared(router);
-
-	/* Create TCP connection provider */
-	auto connectionProvider = oatpp::network::tcp::server::ConnectionProvider::createShared({ "localhost", 8000, oatpp::network::Address::IP_4 });
-
-	/* Create server which takes provided TCP connections and passes them to HTTP connection handler */
-	oatpp::network::Server server(connectionProvider, connectionHandler);
-
-	/* Print info about server port */
-	auto host = static_cast<const char*>(connectionProvider->getProperty("host").getData());
-	auto port = static_cast<const char*>(connectionProvider->getProperty("port").getData());
-	
-	std::ostringstream sWelcome;
-	sWelcome << "APOD API Server running on " << host << ":" << port << std::endl;
-	Logger::get()->info(sWelcome.str());
-
-	/* Run server */
-	server.run();
+App::App(char* argv[]) {
+	this->run(argv);
 }
 
-int main(int argc, char *argv[]) {
-	 bool crash = false;
-   Logger::configureLogger(argv);
-	 Logger::startMessage();
+void App::run(char* argv[]) {
+	configureLogger(argv);
+	configThread();
+	start();
+	idle();
+}
 
-   oatpp::base::Environment::init();
+void App::end() {
+	oatpp::base::Environment::destroy();
+}
 
-   while (true) {
-		 try {
-			 // holds the app awake, but only keeps "heart beat"
-       run();
-			 std::this_thread::sleep_for(std::chrono::seconds(10));
-			 Logger::get()->heartBeat();
-		 }
-		 catch (const std::exception& ex) {
-			 Logger::get()->fatal(std::string(ex.what()));
-			 crash = true;
-		 }
-   }
+void App::configureLogger(char* argv[]) {
+	Logger::configureLogger(argv);
+	Logger::startMessage();
+	logger = Logger::get();
+}
 
-	 oatpp::base::Environment::destroy();
-   
-   return crash;
+void App::configThread() {
+	std::string start = "configuring thread";
+	std::string end = "configuring thread finished";
+	logger->info(start);
+	std::thread t = std::thread([this] {
+		startThread();
+		});
+	logger->info(end);
+
+	t.detach();
+}
+
+void App::startThread() {
+	while (true) {
+		try {
+			std::this_thread::sleep_for(std::chrono::seconds(10));
+		}
+		catch (...) {
+		}
+	}
+}
+
+void App::start() {
+	std::thread([] {
+	ServerComponents components;
+	Logger* log = Logger::get();
+
+	oatpp::network::Server server(
+		components.serverConnectionProvider.getObject(),
+		components.serverConnectionHandler.getObject() //
+	);
+
+	/* Print info about server port */
+	auto serverProvider = components.serverConnectionProvider.getObject();
+	auto host = static_cast<const char*>(serverProvider->getProperty("host").getData());
+	auto port = static_cast<const char*>(serverProvider->getProperty("port").getData());
+
+	std::ostringstream sWelcome;
+	sWelcome << "APOD API Server running on " << host << ":" << port << std::endl;
+	log->info(sWelcome.str());
+
+	/* Run server */
+		server.run();
+		
+		}).detach();
+}
+
+void App::idle() {
+	while (true) {
+		// holds the app awake, but only keeps "heart beat i.e. BEEP"
+		std::this_thread::sleep_for(std::chrono::seconds(5));
+		Logger::get()->heartBeat();
+	}
+}
+
+int main(int argc, char* argv[]) {
+	bool crash = false;
+	App* app{ nullptr };
+
+
+	oatpp::base::Environment::init();
+
+	while (true) {
+		try {
+			app = new App(argv);
+		}
+		catch (const std::exception& ex) {
+			Logger::get()->fatal(std::string(ex.what()));
+			crash = true;
+		}
+	}
+
+	App::end();
+
+	return crash;
 }
